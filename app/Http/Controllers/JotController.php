3e8c561b05231de34d149bad373ed02a905645fb
Tunde Yusuf <yusuphtunday@yahoo.com>
2022-03-21 07:39:48 +0100

    Integrated backend login for AuthX.

--- app/Http/Controllers/JotController.php
+++ app/Http/Controllers/JotController.php
@@ -3,15 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 use App\Models\Jot;
+use App\Models\User;
 
 class JotController extends Controller
 {
 
     public function sign_in(Request $request)
     {
-        dd('login successful.', $request->all());
+        $user_code = $request->code;
+        $api_url = env('AUTHX_API_URL');
+        $app_code = env('AUTHX_APPLICATION_CODE');
+
+        $response = Http::withHeaders([
+            'Api-Token' => env('AUTHX_API_TOKEN'),
+        ])->get("{$api_url}/apps/{$app_code}/users/{$user_code}");
+
+        if(!$response->successful())
+            return redirect('/')->with('error', 'Unable to complete sign on process at this time, kindly try again in a few seconds.');
+
+        $response_body = $response->json()->data->main_data->data;
+
+        $params = [
+            'name' => $response_body->first_name.' '.$response_body->last_name,
+            'password' => Hash::make(\Str::rand(111111, 999999)),
+            'authx_token' => $user_code,
+        ];
+
+        $user = User::firstOrCreate(['email' => $response_body->email], $params);
+
+        Auth::loginUsingId($user->id);
+
+        return redirect('/')->with('success', 'User successfully logged in.');
     }
 
     public function home(Request $request)
